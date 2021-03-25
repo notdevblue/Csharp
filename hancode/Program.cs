e0ebe5347ca249924f7b22405ed356bd3d9a5928
han at class <wooyeophan@gmail.com>
2021-03-25 15:40:46 +0900

    C# 6교시

--- hancode/Program.cs
+++ hancode/Program.cs
@@ -6,9 +6,10 @@ namespace hancode
     {
         static void Main(string[] args)
         {
+            #region 4교시
+
             //오이런 계속 타이핑 하고 제출해야 했는데 지워버렸네요
             //유니코드 알아보는 곧에서 깨달았
-
 
             //char c = '쥬';
             //char begin = '가';
@@ -23,19 +24,162 @@ namespace hancode
             //Console.WriteLine(s);
             //string test1 = " \"Hello\" ";
             //Console.WriteLine(test1 + " \"world\" ");
-            Console.WriteLine("Hello"[0]);
-            Console.WriteLine("Hello"[1]);
-            Console.WriteLine("Hello"[2]);
-            Console.WriteLine("Hello"[3]);
-            Console.WriteLine("Hello"[4]);
+            //Console.WriteLine("Hello"[0]);
+            //Console.WriteLine("Hello"[1]);
+            //Console.WriteLine("Hello"[2]);
+            //Console.WriteLine("Hello"[3]);
+            //Console.WriteLine("Hello"[4]);
             //Console.WriteLine("Hello"[5]);
+            #endregion
+
+            #region 5교시
+            //Console.WriteLine("와센즈".Length);
+            //Console.WriteLine('가' + '힣');
+
+            //string s1 = "C#";
+            //string s2 = "Programming";
+            //char c = s2[1];
+
+            //string s3 = s1 + " " + s2; Console.WriteLine(s3);
+            //string s4 = s2.Substring(1, 3); Console.WriteLine(s4);
+            //string s5 = s2.Replace("Pro", "PRO"); Console.WriteLine(s5);
+            //Console.WriteLine(s2.Remove(3));
+            //Console.WriteLine("        H e l l o  ".Trim());
+            //Console.WriteLine(s2.ToLower());
+            //Console.WriteLine(s2.ToUpper());
+
+            //int a = 1;
+            //long l = 1000L;
+            //float f = 1.0f;
+            //double d = 1.0;
+            //decimal m = 10000m;
+            //string str = "안녕하세요";
+            //Console.WriteLine(sizeof(int));
+            //Console.WriteLine(sizeof(short));
+            //Console.WriteLine(sizeof(long));
+            //Console.WriteLine(sizeof(float));
+            //Console.WriteLine(sizeof(double));
+            //Console.WriteLine(sizeof(decimal));
+            //Console.WriteLine(sizeof(byte));
+            //Console.WriteLine(sizeof(char));
+            //Console.WriteLine(sizeof(bool));
+
+            //bool b = true;
+            //Console.WriteLine(1 > 2);
+            //Console.WriteLine(true);
+            //Console.WriteLine(false);
+
+            //int a = 0;
+            //a += 1;
+            //a *= 10;
+            //a /= 2;
+            //a -= 1;
+
+            //int a = 0;
+            //++a;
+            //a++;
+            //--a;
+            //a--;
+
+
+            //int number = 10;
+            //Console.WriteLine(number);
+            //Console.WriteLine(number++);
+            //Console.WriteLine(number--);
+            //Console.WriteLine(number);
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(++number);
+            //Console.WriteLine(--number);
+            //Console.WriteLine(number);
+
+
+            //int _int = 273;
+            //long _long = 522731033265;
+            //float _float = 52.273f;
+            //double _double = 52.273;
+            //char _char = '글';
+            //string _string = "문자열";
+
+            //Console.WriteLine(_int.GetType());
+            //Console.WriteLine(_long.GetType());
+            //Console.WriteLine(_float.GetType());
+            //Console.WriteLine(_double.GetType());
+            //Console.WriteLine(_char.GetType());
+            //Console.WriteLine(_string.GetType());
+            #endregion
+
+            #region 6교시
+            //var number = 100;
+
+            //string s = Console.ReadLine();
+            //Console.WriteLine(s);
+
+            //int a = 0;
+            //long b = a;
+            //int c = (int)b;
+
+            //Console.WriteLine(int.Parse("52"));
+            //Console.WriteLine(long.Parse("273"));
+            //Console.WriteLine(float.Parse("52.273"));
+            //Console.WriteLine(double.Parse("103.32"));
+
+            //Console.WriteLine(int.Parse("52").GetType());
+            //Console.WriteLine(long.Parse("273").GetType());
+            //Console.WriteLine(float.Parse("52.273").GetType());
+            //Console.WriteLine(double.Parse("103.32").GetType());
+
+            //Console.WriteLine("계산기");
+            //string num1;
+            //string num2;
+            //string calc;
+            //float result = 0;
+
             
 
+            //num1 = Console.ReadLine();
+            //calc = Console.ReadLine();
+            //num2 = Console.ReadLine();
+
+            //if (num2 == "박제인" && calc == "/")
+            //{
+            //    Console.WriteLine("0으로 나눌 수 없습니다.");
+            //    return;
+            //}    
+
+            //switch(calc)
+            //{
+            //    case "+":
+            //        result = float.Parse(num1) + float.Parse(num2);
+            //        break;
+
+            //    case "-":
+            //        result = float.Parse(num1) - float.Parse(num2);
+            //        break;
+
+            //    case "/":
+            //        result = float.Parse(num1) / float.Parse(num2);
+            //        break;
+
+            //    case "*":
+            //        result = float.Parse(num1) * float.Parse(num2);
+            //        break;
+            
+            //    default:
+            //        Console.WriteLine("? 머임");
+            //        break;
+            //}
+            
+            //Console.WriteLine(result.ToString("0.00"));
+            //Console.WriteLine(string.Format("{0:X}", 1000));
+
+
+            #endregion
         }
 
-        class ClassA
-        {
-            public int level = 0;
-        }
+        //class ClassA
+        //{
+        //    public int level = 0;
+        //}
     }
 }
