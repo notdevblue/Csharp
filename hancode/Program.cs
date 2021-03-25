252a778fdd9a5f3f6346f916bf067b20d2bc37b8
han at class <wooyeophan@gmail.com>
2021-03-25 13:36:49 +0900

    초기 커밋.

---
+++ hancode/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace hancode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //오이런 계속 타이핑 하고 제출해야 했는데 지워버렸네요
+            //유니코드 알아보는 곧에서 깨달았
+
+
+            //char c = '쥬';
+            //char begin = '가';
+            //char end = '힣';
+            //Console.WriteLine(c);
+            //Console.WriteLine((int)char.MaxValue);
+            //Console.WriteLine((int)c);
+            //Console.WriteLine((int)begin);
+            //Console.WriteLine((int)end);
+
+            //string s = "\"helll\\\t\\lllo\"\n";
+            //Console.WriteLine(s);
+            //string test1 = " \"Hello\" ";
+            //Console.WriteLine(test1 + " \"world\" ");
+            Console.WriteLine("Hello"[0]);
+            Console.WriteLine("Hello"[1]);
+            Console.WriteLine("Hello"[2]);
+            Console.WriteLine("Hello"[3]);
+            Console.WriteLine("Hello"[4]);
+            //Console.WriteLine("Hello"[5]);
+            
+
+        }
+
+        class ClassA
+        {
+            public int level = 0;
+        }
+    }
+}
