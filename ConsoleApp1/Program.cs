7d1334fe10f6b3e1b12fd434ad0d4ea7951eacab
han at 2f <wooyeophan@gmail.com>
2021-05-13 12:59:08 +0900

    4교시

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_5_13
+{
+    class Program
+    {
+        #region 4교시
+
+        //class Box
+        //{
+        //    public int Width { get; set; }
+        //    public int Height { get; set; }
+        //}
+
+        //class CTest
+        //{
+        //    public int a;
+        //}
+
+        //static void Method(CTest a)
+        //{
+        //    a.a = 2;
+        //}
+        //static void Method1(int a)
+        //{
+        //    a = 2;
+        //}
+        #endregion
+
+        static void Main(string[] args)
+        {
+            #region 4교시
+
+            //Box box = new Box();
+            //box.Width = 10;
+            //box.Height = 154;
+            //Console.WriteLine(box.Width);
+            //Console.WriteLine(box.Height);
+
+            //int a = 0;
+            //int b = a;
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //CTest c1 = new CTest();
+            //CTest c2 = new CTest();
+            //c2.a = c1.a;
+
+            //c1.a = 10;
+            //Console.WriteLine($"c1: {c1.a}");
+            //Console.WriteLine($"c2: {c2.a}");
+            //c2.a = 154;
+
+            //Console.WriteLine($"c1: {c1.a}");
+            //Console.WriteLine($"c2: {c2.a}");
+
+            //int a = 0;
+            //CTest c1 = new CTest();
+            //Method(c1);
+            //Method1(a);
+            //Console.WriteLine(c1.a);
+            //Console.WriteLine(a);
+            
+            
+
+            #endregion
+        }
+    }
+}
