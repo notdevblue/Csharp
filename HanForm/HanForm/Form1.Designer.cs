768e64e23a68282ea95433e4c0c046322fd2b896
han at 2f <wooyeophan@gmail.com>
2021-06-17 12:58:58 +0900

    생각보다 재미있는 C# 윈폼 / 6월 17일

---
+++ HanForm/HanForm/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace HanForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.myText = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.myTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(114, 232);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "OK";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 193);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // myText
+            // 
+            this.myText.AutoSize = true;
+            this.myText.Font = new System.Drawing.Font("문체부 궁체 정자체", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.myText.Location = new System.Drawing.Point(114, 115);
+            this.myText.Name = "myText";
+            this.myText.Size = new System.Drawing.Size(85, 15);
+            this.myText.TabIndex = 3;
+            this.myText.Text = "새로운 시작";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 155);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // myTimer
+            // 
+            this.myTimer.Enabled = true;
+            this.myTimer.Interval = 1000;
+            this.myTimer.Tick += new System.EventHandler(this.myTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.myText);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label myText;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer myTimer;
+    }
+}
+
---
+++ HanForm/HanForm/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HanForm
+{
+    public partial class Form1 : Form
+    {
+        private int btnClickCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            myText.Text = "내 콤퓨타 관리자";
+            textBox1.PlaceholderText = "관리자통과어";
+            textBox1.PasswordChar = '별';
+            textBox2.PlaceholderText = "고유알림어";
+            btnLogin.Text = "들어가기";
+            myText.MouseHover += new EventHandler(onTextHover);
+
+            btnLogin.Width = textBox1.Width;
+            btnLogin.Height = textBox1.Height;
+        }
+
+        private void onTextHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("내 콤퓨타 관리자 입니다.");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            btnLogin.Text = "어림도없지";
+
+            for(int i = 1; i < 5; ++i)
+            {
+                Button b = new Button();
+                b.Width = 150;
+                b.Location = new Point(400, 100 + i * 30);
+                b.Text = $"{i} 번째 버튼";
+                b.Click += new EventHandler(onNewBtnClick);
+                b.DoubleClick += new EventHandler(onNewBtnDoubleClick);
+                Controls.Add(b);
+            }
+
+        }
+
+        private void onNewBtnClick(object sender, EventArgs e)
+        {
+            ++btnClickCount;
+            Button b = sender as Button;
+
+            b.Text = "눌렀네";
+
+            myText.Text = $"새로 생긴 버튼을 총 {btnClickCount} 번 눌렀습니다";
+
+            if(btnClickCount > 10)
+            {
+                // do something
+            }
+            
+            //MessageBox.Show("와센즈");
+        }
+
+        private void onNewBtnDoubleClick(object sender, EventArgs s)
+        {
+            Button b = sender as Button;
+
+            b.Text = "오 더블클릭";
+        }
+
+
+        int sec = 0;
+        private void myTimer_Tick(object sender, EventArgs e)
+        {
+            ++sec;
+            myText.Text = $"{sec} 초가 지났습니다.";
+        }
+    }
+}
